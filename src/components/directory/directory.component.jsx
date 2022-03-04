ae2093e0439266025659d6cf67851690fefde6df
Balbader <15971200+Balbader@users.noreply.github.com>
2022-03-04 12:27:29 +0100

    add size prop

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,7 +47,7 @@ class Directory extends React.Component {
 			<div className='directory-menu'>
 				{
 					this.state.sections.map(({title, imageUrl, id, size}) => (
-						<MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+						<MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size}/>
 					))
 				}
 			</div>
