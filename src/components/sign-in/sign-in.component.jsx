99ba0ad2e5a6e24b3c6224c81cec55b242ddb31d
Balbader <15971200+Balbader@users.noreply.github.com>
2022-03-08 13:42:57 +0100

    add google sign in logic + style google sign in button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -53,7 +53,9 @@ class SignIn extends React.Component {
             required
           />
           <CustomButton type="submit">Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>Sign In with Google</CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            Sign In with Google
+          </CustomButton>
         </form>
       </div>
     )
