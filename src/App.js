b368d1545c220aaac602c285c6bb21c692a1fae4
Balbader <15971200+Balbader@users.noreply.github.com>
2022-03-11 12:36:02 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -33,7 +33,7 @@ class App extends React.Component {
           });
         });
       } else {
-        this.setState({currentUser: userAuth});
+        this.setState({ currentUser: userAuth });
       }
     });
   }
